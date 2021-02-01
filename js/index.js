4b268912c0cd34a7667b2a264641f2166e8a71a5
ronronscelestes <julie.plantey@gmail.com>
2021-02-01 20:26:06 +0100

    adding hobbies html/css/js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log("foo");
+let julieWidget = document.querySelector('.julie-widget');
+let matthieuWidget = document.querySelector('.matthieu-widget');
+let julieName = document.querySelector('.julieName');
+let matthieuName = document.querySelector('.matthieuName');
+
+julieName.addEventListener('click', event => {
+    julieWidget.classList.remove('hide');
+    matthieuWidget.classList.add('hide');
+})
+
+matthieuName.addEventListener('click', event => {
+    matthieuWidget.classList.remove('hide');
+    julieWidget.classList.add('hide');
+})
