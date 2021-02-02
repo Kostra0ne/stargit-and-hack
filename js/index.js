43dcb68899ab4ba168c178f47d406f0dd916bb43
ronronscelestes <julie.plantey@gmail.com>
2021-02-02 22:10:09 +0100

    fixing the merge issue

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
 let julieWidget = document.querySelector('.julie-widget');
 let matthieuWidget = document.querySelector('.matthieu-widget');
+let welcomeWidget = document.querySelector('.welcome-container');
+
 let julieName = document.querySelector('.julieName');
 let matthieuName = document.querySelector('.matthieuName');
 
 julieName.addEventListener('click', event => {
     julieWidget.classList.remove('hide');
     matthieuWidget.classList.add('hide');
-})
+    welcomeWidget.classList.add('hide');
+    console.log("event listener julie");
+});
 
 matthieuName.addEventListener('click', event => {
     matthieuWidget.classList.remove('hide');
     julieWidget.classList.add('hide');
-})
+    welcomeWidget.classList.add('hide');
+    console.log("event listener matthieu");
+});
+
